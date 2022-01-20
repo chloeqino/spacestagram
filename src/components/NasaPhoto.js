548d32423e4f51f68f70fdc5f529a614b3cc797b
Qinyu Chen <wendychen0812@gmail.com>
2022-01-19 22:41:54 -0800

    update

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -62,7 +62,7 @@ if (!photoData) return <div>Loading</div>;
         <p className="explaination">{photoData.explanation}</p>
      
       <div className="useraction">
-      <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 45 45"><path 
+      <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="-3 0 45 45"><path 
       onClick = {(e)=>{setLiked(!liked);
       handleLiked()}} className={`heart ${liked ? "fill":""}`} stroke="black" d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z"/></svg>
 <a className="twitter-share-button"
